c75af86876ae8d5d375eee8853e93d1087426dbf
magdalenaatzl <1810653965@stud.fh-kufstein.ac.at>
2018-11-22 05:53:23 +0100

    Uebung3

---
+++ src/fhkufstein/Aufgabe3_1810653965.java
@@ -0,0 +1,30 @@
+package fhkufstein;
+
+public class Aufgabe3_1810653965
+{
+    public static void main(String[] args)
+    {
+    //deklariern und Wertzuweisung
+    int zahl1 = 7;
+    int zahl2 = 10;
+    int zahl3 = 5;
+    double zahl4 = 1.7;
+    double zahl5 = 8.9;
+    double zahl6 = 4.8;
+
+    int ergebnis1 = zahl1 + zahl2; //addieren
+    double ergebnis2 = zahl1 / zahl3; //dividieren
+    double ergebnis3 = zahl4 * zahl2; //multiplizieren
+    double ergebnis4 = zahl1 % zahl2; //Modulo/Rest berechnen
+    int ergebnis5 = ++zahl3; //Inkrementieren
+    int ergebnis6 = --zahl2; //Dekrementieren
+
+    //Ausgabe in der Konsole
+        System.out.println(ergebnis1);
+        System.out.println(ergebnis2);
+        System.out.println(ergebnis3);
+        System.out.println(ergebnis4);
+        System.out.println(ergebnis5);
+        System.out.println(ergebnis6);
+    }
+}
