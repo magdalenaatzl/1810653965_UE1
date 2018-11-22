81b51840cfd32ffc01c43dbbd2d8a78a33a06615
magdalenaatzl <1810653965@stud.fh-kufstein.ac.at>
2018-11-22 05:28:53 +0100

    Uebung2

---
+++ src/fhkufstein/Main.java
@@ -0,0 +1,75 @@
+package fhkufstein;
+
+public class Main {
+
+    public static void main(String[] args) {
+    //deklarieren von VAriablen
+
+	String name;
+	int zahl1;
+	double zahl2;
+	boolean wahrheitswert;
+	long zahl3;
+	char zeichen;
+	byte zahl4;
+	short zahl5;
+	float zahl6;
+
+	//Wertzuweisung
+
+	name = "Magdalena";
+	//für Zeichenketten
+
+	zahl1 = 254524352;
+	/*Ganzzahl
+	Wertebereich: -2.147.483.648 bis 2.147.483.647
+	Größe: 4 Byte
+	 */
+
+	zahl2 = 23.23423423234;
+	/*Gleitkommazahl mit 15 Nachkommastellen
+	Wertebereich: 1,4*E-324 bis 1.7*E308
+	Größe: 8 Byte
+	 */
+
+	wahrheitswert = true;
+	/*Wahrheitswert
+	Wertebereich: true oder false
+	Größe: 1 Byte
+	 */
+
+	zahl3 = 809775806;
+	/*Ganzzahl
+	Wertebereich: -9.223.372.036.854.775.808 bis 9.223.372.036.854.775.807
+	Größe: 8 Byte
+	 */
+
+	zeichen = 0x00BD;
+	//für einzelne Zeichen und Unicode-Zeichen
+
+	zahl4 = 120;
+	/*Ganzzahl
+	Wertebereich: -128 bis 127
+	Größe: 1
+	 */
+
+	zahl5 = 32767;
+	/*Ganzzahl
+	Wertebereich: -32.768 bis 32767
+	Größe: 2
+	 */
+
+	zahl6 =.54523f;
+	/*Gleitkommazahl mit 7 Nachkommastellen
+	Wertebereich: 1.4*E-45 bis 3.4*E18
+	Größe: 4 Byte
+	 */
+
+	final int zahl = 218;
+	/*Konstanten sind Variablen die nach einmaligen Initialisieren nicht verändert
+	werden können. Man verwendet sie um oft verwendete Variablen nicht unnötig mehrfach zu deklarieren.
+	 */
+
+
+    }
+}
