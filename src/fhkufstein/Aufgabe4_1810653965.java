403d9013afb7fe557587646ec90c8729b7798ad2
magdalenaatzl <1810653965@stud.fh-kufstein.ac.at>
2018-11-22 06:17:54 +0100

    Uebung4

---
+++ src/fhkufstein/Aufgabe4_1810653965.java
@@ -0,0 +1,22 @@
+package fhkufstein;
+
+public class Aufgabe4_1810653965 {
+    public static void main(String[]args)
+    {
+    //Deklariern und Wertzuweisung der Variablen
+    int zahl1 = 10;
+    int zahl2 = 5;
+
+    //Ausgabe der Ergebnisse
+        System.out.println("Die Zahl1 ist kleiner als die Zahl2: " + (zahl1<zahl2));
+        System.out.println("Die Zahl2 ist kleiner als die Zahl1: " + (zahl2<zahl1));
+        System.out.println("Die Zahl1 ist größer als die Zahl2: "+(zahl1>zahl2));
+        System.out.println("Die Zahl2 ist größer als die Zahl1: "+(zahl2>zahl1));
+        System.out.println("Die Zahl1 ist kleiner oder gleich als die Zahl2: "+(zahl1<=zahl2));
+        System.out.println("Die Zahl2 ist kleiner oder gleich als die Zahl1: "+(zahl2<=zahl1));
+        System.out.println("Die Zahl1 ist größer oder gleich als die Zahl2: "+(zahl1>=zahl2));
+        System.out.println("Die Zahl2 ist größer oder gleich als die Zahl1: "+(zahl2>=zahl1));
+        System.out.println("Die Zahl1 ist gleich als die Zahl2: "+(zahl1==zahl2));
+        System.out.println("Die Zahl1 ist nicht gleich als die Zahl2: "+(zahl1!=zahl2));
+    }
+}
